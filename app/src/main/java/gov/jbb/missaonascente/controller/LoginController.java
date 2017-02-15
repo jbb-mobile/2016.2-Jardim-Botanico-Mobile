84c1ea90367c8a115042ff8777f7c27e33c99688
João Vitor Araujo <joao18araujo@gmail.com>
2017-02-15 10:11:39 -0200

    Changing login controller

--- app/src/main/java/gov/jbb/missaonascente/controller/LoginController.java
+++ app/src/main/java/gov/jbb/missaonascente/controller/LoginController.java
@@ -3,6 +3,13 @@ package gov.jbb.missaonascente.controller;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import gov.jbb.missaonascente.dao.ExplorerDAO;
 import gov.jbb.missaonascente.dao.LoginRequest;
 import gov.jbb.missaonascente.model.Explorer;
@@ -16,6 +23,9 @@ public class LoginController {
     private boolean action = false;
     private boolean response;
     private static final String PREF_NAME = "MainActivityPreferences";
+
+    private DatabaseReference rootReference = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference explorersReference = rootReference.child("explorers");
 
     public LoginController() {
         explorer = new Explorer();
@@ -38,8 +48,8 @@ public class LoginController {
         return true;
     }
 
-    public void doLogin(final String email, String password, final Context context){
-        Explorer explorerFromLogin = new Explorer();
+    public void doLogin(final String email, final String password, final Context context){
+        final Explorer explorerFromLogin = new Explorer();
         explorerFromLogin.setEmail(email);
 
         try {
@@ -48,7 +58,32 @@ public class LoginController {
             e.printStackTrace();
         }
 
-        LoginRequest loginRequest = new LoginRequest(explorerFromLogin.getEmail(), explorerFromLogin.getPassword());
+        DatabaseReference newExplorerReference = explorersReference.child(explorerFromLogin.getFirebaseEmail());
+
+        newExplorerReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                setResponse(dataSnapshot.exists());
+                if (dataSnapshot.exists()) {
+                    Explorer explorer = dataSnapshot.getValue(Explorer.class);
+                    if (explorer.getPassword().equals(explorerFromLogin.getPassword())) {
+                        saveFile(email, context);
+                    }else{
+                        setResponse(false);
+                    }
+
+                } else {
+                    Log.d("DB", "Usuário não existe");
+                }
+
+                setAction(true);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError){
+            }
+        });
+
+       /* LoginRequest loginRequest = new LoginRequest(explorerFromLogin.getEmail(), explorerFromLogin.getPassword());
         loginRequest.request(context, new LoginRequest.Callback() {
             @Override
             public void callbackResponse(boolean response) {
@@ -56,17 +91,13 @@ public class LoginController {
                 if(response){
                     saveFile(email, context);
 
-                    /*MainController mainController = new MainController();
-                    mainController.checkIfUpdateIsNeeded(context);*/
-
                     //----------------------------------------------------------------
                     new ExplorerController().updateElementExplorerTable(context, email);
                     new AchievementController(context).updateAchievementExplorerTable(context, email);
                 }
                 setAction(true);
             }
-        });
-
+        });*/
     }
 
     public boolean isAction() {
